3257a7f8017d4636636ef6c8900a6103031fa962
DiabGarren <garrendiab@gmail.com>
2022-10-08 23:25:17 +0200

    Added some stuff

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,6 +1,32 @@
-date = new Date;
-year = date.getFullYear();
+const date = new Date;
+const year = date.getFullYear();
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full"
+}).format(
+    now
+);
 
-if(document.getElementById("year").innerHTML != null){
+if (document.getElementById("year").innerHTML != null) {
     document.getElementById("year").innerHTML = year;
+    document.getElementById("updated").innerHTML = `Last updated ${document.lastModified}`;
+    document.getElementById("date").innerHTML = fulldate;
 }
+
+function toggleMenu() {
+    nav = document.getElementById("primary_nav");
+    if (nav.style.display == "none" || nav.style.display == "") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+
+    if (hamBtn.innerHTML == "☰" || hamBtn.innerHTML == "&#9776;") {
+        hamBtn.innerHTML = "X";
+    } else {
+        hamBtn.innerHTML = "☰";
+    }
+}
+
+const hamBtn = document.querySelector("#ham_btn");
+hamBtn.addEventListener("click", toggleMenu);
