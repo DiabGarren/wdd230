fd7b97810330e8a3d135a55c69fff1d057e985d5
DiabGarren <garrendiab@gmail.com>
2022-10-28 03:39:47 +0200

    Added images

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -30,21 +30,69 @@ if (now.getDay() == 1 || now.getDay() == 2) {
     meeting.innerHTML = "<h2>Wednesday Meeting!</h2><p>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</p>"
 }
 
-const temp = document.querySelector("#temp").textContent;
-const windSpeed = document.querySelector("#wind-speed").textContent;
-const windChill = document.querySelector("#wind-chill");
+if (document.querySelector("#temp") == null) {} else {
+    const temp = document.querySelector("#temp").textContent;
+    const windSpeed = document.querySelector("#wind-speed").textContent;
+    const windChill = document.querySelector("#wind-chill");
 
-const calcWindChill = (temp, windSpeed) => {
-    if (temp > 10 || windSpeed < 4.8) {
-        return "N/A";
+    const calcWindChill = (temp, windSpeed) => {
+        if (temp > 10 || windSpeed < 4.8) {
+            return "N/A";
+        } else {
+            temp = (temp * 9 / 5) + 32;
+            windSpeed = windSpeed / 1.609
+
+            let windChill = 35.75 + 0.6215 * temp - 35.75 * windSpeed ** 0.16 + 0.4275 * temp * windSpeed ** 0.16
+            windChill = (windChill - 32) * 5 / 9;
+            return `${windChill.toFixed(2)}°C`;
+        }
+    }
+
+    windChill.textContent = calcWindChill(temp, windSpeed);
+}
+
+if (document.querySelector("img[data-src]") == null) {} else {
+    let imagesToLoad = document.querySelectorAll("img[data-src]");
+    const loadImages = (image) => {
+        image.setAttribute("src", image.getAttribute("data-src"));
+        image.onload = () => {
+            image.removeAttribute("data-src");
+        };
+    };
+
+    if ("IntersectionObserver" in window) {
+        const observer = new IntersectionObserver((items, observer) => {
+            items.forEach((item) => {
+                if (item.isIntersecting) {
+                    loadImages(item.target);
+                    observer.unobserve(item.target);
+                }
+            });
+        });
+
+        imagesToLoad.forEach((img) => {
+            observer.observe(img);
+        });
+
     } else {
-        temp = (temp*9/5) + 32;
-        windSpeed = windSpeed/1.609
-
-        let windChill = 35.75 + 0.6215*temp - 35.75*windSpeed**0.16 + 0.4275*temp*windSpeed**0.16
-        windChill = (windChill - 32)*5/9;
-        return `${windChill.toFixed(2)}°C`;
+        imagesToLoad.forEach((img) => {
+            loadImages(img);
+        });
     }
 }
 
-windChill.textContent = calcWindChill(temp, windSpeed);
+
+const visit = document.querySelector("#visit");
+
+let lastVisit = window.localStorage.getItem("lastVisitTime");
+
+if (lastVisit == null) {
+    visit.textContent = "This is your first visit!";
+    console.log()
+} else {
+    visit.textContent = Math.round((Date.now() - lastVisit) / 86400000);
+}
+
+let visitTime = Date.now();
+
+localStorage.setItem("lastVisitTime", visitTime);
